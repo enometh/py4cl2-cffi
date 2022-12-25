729489b00f1587987c10fd5821f96040dcd8d8e9
Madhu <enometh@net.meer>
2022-12-25 12:32:59 +0530

    src/config.lisp: defvars on load in src/{config/shared-objects}.lisp:
    * src/config.lisp: edit +default-python+ here to set "python3.9" say
    * src/shared-objects.lisp: use +default-python+ to load numpy.
    
    [240520]
    * src/config.lisp: (return-value-as-list) signature arglist modified
    to take an additional argument override-default-python, so other users
    can be made to work (by changing the code to supply it.
    
    [241208]
    
    * src/config.lisp: (+default-python+): keep as nil
    (+python-version-string+): make it a defparameter instead of
    defconstant.  (*python-includes*,
    *python-executable-path*,*python-site-packages-path*): make these
    defparameters instead of defvars
    
    this should support reloading:
    ```
    (mk:oos  "py4cl2-cffi/config" :load :compile-during-load nil
    :load-source-instead-of-binary t)
    (defparameter py4cl2-cffi/config::+default-python+ "python3.11")
    (mk:oos  "py4cl2-cffi/config" :load :compile-during-load nil
    :load-source-instead-of-binary t)
    py4cl2-cffi/config::*python-includes*
    (mk:mk-oos :py4cl2-cffi)
    ```

--- src/config.lisp
+++ src/config.lisp
@@ -14,6 +14,16 @@
 
 (in-package :py4cl2-cffi/config)
 
+#+nil
+(mapcar (lambda (x)
+	  (let ((sym (find-symbol (symbol-name x) :py4cl2-cffi/config)))
+	    (when sym
+	      (unintern sym :py4cl2-cffi/config))))
+	'(*python-ldflags*
+	  *python-includes*
+	  *python-compile-command*))
+
+
 ;; Use python3-config or equivalent to discover these values
 
 ;; TODO: Could set up better defaults for different OS
@@ -45,7 +55,20 @@ The second ~A corresponds to the numpy include files discovered
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
 
-  (defun return-value-as-list (cmd)
+  (defvar +default-python+ nil
+    "If NON-NIL should be a string like \"python3.9\" which
+is used instead of python3 or python3.9-config.")
+
+  (defun return-value-as-list (cmd &optional override-+default-python+)
+    "If OVERRIDE-DEFAULT-PYTHON is non-NIL, skip the codepath that kludges
+the commandline to use +DEFAULT-PYTHON+."
+    (when (and +default-python+ (not override-+default-python+))
+      (let* ((prefix "python3")
+	     (len (length prefix)))
+	(assert (string= prefix cmd :end2 len))
+	(setq cmd (with-output-to-string (stream)
+		    (write-string +default-python+ stream)
+		    (write-string cmd stream :start len)))))
     (remove ""
             (mapcar (lambda (value)
                       (string-trim '(#\newline) value))
@@ -56,9 +79,12 @@ The second ~A corresponds to the numpy include files discovered
                      :separator '(#\newline #\tab #\space)))
             :test #'string=))
 
+  #+nil ;; redefine +default-python+
   (alexandria:define-constant +python-version-string+
     (second (return-value-as-list "python3 --version"))
     :test #'string=)
+  (defparameter +python-version-string+
+    (second (return-value-as-list "python3 --version")))
 
   (if (uiop:version< +python-version-string+ "3.8.0")
       (defvar *python-ldflags*
@@ -67,16 +93,23 @@ The second ~A corresponds to the numpy include files discovered
         (return-value-as-list "python3-config --embed --ldflags"))))
 
 
-(defvar *python-includes*
+(defparameter *python-includes*
   (return-value-as-list "python3-config --includes"))
 
-(defvar *python-executable-path*
-  (first (return-value-as-list "which python3"))
+
+#||
+HARDCODE
+(defvar *python-ldflags* (return-value-as-list "python3.9-config --embed --ldflags" t))
+(defvar *python-includes* (return-value-as-list "python3.9-config --includes" t))
+||#
+
+(defparameter *python-executable-path*
+  (first (return-value-as-list "which python3" t))
   "The path to python executable. This will be used to set sys.path.
 This is useful in cases such as venv when python3-config does not lead
 to expected paths.")
 
-(defvar *python-site-packages-path*
+(defparameter *python-site-packages-path*
   (return-value-as-list
    "python3 -c 'import sys; print(\"\\n\".join(sys.path))'"))
 
